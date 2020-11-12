a2d42c3655948c2153284f40b420ba1008067194
madiwa Simon <mad.simon01@gmail.com>
2020-11-12 16:14:59 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,13 +2,13 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {},
-      :matiarch => {},
+      :matriarch => {},
       :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
       :patriarch => {},
-      :matiarch => {},
+      :matriarch => {},
       :heroine => {},
       :heroine_friends => []
       }}
